0d2275ba0bdbaa2d84769cf4caaf6bb5ec5e837f
your-name <your-email>
2019-03-19 14:30:11 +0800

    change

--- rectangle.js
+++ rectangle.js
@@ -1,81 +1,18 @@
+/* global Rectangle: true */
 $(function() {
-    var $width   = $('#rectangle-width'),
-      $height  = $('#rectangle-height'),
-      $calc    = $('#rectangle-calc'),
-      blnValid = false;
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
 
-$calc.click(function() {
-      if(!blnValid) return;
+  $btnCal.click(function(){
+    var w = $width.val(),
+        h = $height.val();
 
-          var width  = Number($width.val()),
-          height = Number($height.val()),
-          p      = roundFractional(width * 2 + height * 2, 2),
-          a      = roundFractional(width * height, 2);
+    var r = new Rectangle(w, h);
 
-    $('#rectangle-perimeter').val(p);
-        $('#rectangle-area').val(a);
-          
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
+  });
 });
-
-$width.focusout(function() {
-      blnValid = validate('#rectangle-width');
-        
-});
-  
-$height.focusout(function() {
-      blnValid = validate('#rectangle-height');
-        
-});
-  
-  /**
-   *    * 小数点后面保留第 n 位
-   *       * 
-   *          * @param x 做近似处理的数
-   *             * @param n 小数点后第 n 位
-   *                * @returns 近似处理后的数 
-   *                   */
-function roundFractional(x, n) {
-      return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-        
-}
-
-  /**
-   *    * 对字段进行数据合法性校验
-   *       *
-   *          * @param field 字段的 id
-   *             * @returns boolean 验证通过为 true，验证不通过为 false
-   *                */
-function validate(field) {
-      var $data    = $(field),
-                  $message = $(field + '-validate'),
-                          label    = $(field).attr('data-label');
-
-      if($data.val() === '') {
-              $message.html(label + '不能为空！');
-                    $data.select();
-                          return false;
-                              
-      }
-
-      if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())) {
-              $message.html(label + '必须是数值');
-                    $data.select();
-                          return false;
-                              
-      }
-
-      if(window.Number($data.val()) < 0) {
-              $message.html(label + '必须大于零');
-                    $data.select();
-                          return false;
-                              
-      }
-
-          $message.html('');
-              return true;
-                
-}
-
-});
-
-
