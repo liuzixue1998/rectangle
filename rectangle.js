65df8e72d0a6f01056d94139391c0871fb71a393
liuzixue1998 <156150671@qq.com>
2019-04-15 14:38:11 +0800

    change error

--- rectangle.js
+++ rectangle.js
@@ -4,9 +4,49 @@ $(function() {
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $area = $('#area');
+      $area = $('#area'), 
+      $widthValidate = $('#width-validate'), 
+      $heightValidate = $('#height-validate'), 
+      isPassValidate = false; 
+
+$width.focusout(function() {
+      var result = validate($width.val());
+      isPassValidate = result.isOK;
+      if(!result.isOK) {
+          $widthValidate.html('宽度' + result.reason);
+              $width.select();
+                            
+      } else {
+          $widthValidate.html('');
+                      
+          }
+            
+});
+
+$width.keypress(function(e) {
+  if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+      e.preventDefault();
+              
+  }
+    
+});
+
+$height.focusout(function() {
+      var result = validate($height.val());
+      isPassValidate = result.isOK;
+      if(!result.isOK) {
+          $heightValidate.html('高度' + result.reason);
+              $height.select();
+                            
+      } else {
+          $heightValidate.html('');
+                      
+          }
+            
+});
 
   $btnCal.click(function(){
+    if(!isPassValidate) return;
     var w = $width.val(),
         h = $height.val();
 
