693c1d98270839e7ec6bd3f94fecb4ece06fcc0c
liuzixue1998 <156150671@qq.com>
2019-04-15 14:44:35 +0800

    bugbug

--- rectangle.js
+++ rectangle.js
@@ -1,52 +1,51 @@
-/* global Rectangle: true */
+/* global Rectangle, validate, isLegalKey: true */
 $(function() {
   var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $area = $('#area'), 
-      $widthValidate = $('#width-validate'), 
-      $heightValidate = $('#height-validate'), 
-      isPassValidate = false; 
+      $area = $('#area'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
+      isPassValidate = false;
 
-$width.focusout(function() {
-      var result = validate($width.val());
-      isPassValidate = result.isOK;
-      if(!result.isOK) {
-          $widthValidate.html('宽度' + result.reason);
-              $width.select();
-                            
-      } else {
-          $widthValidate.html('');
-                      
-          }
-            
-});
+  $width.focusout(function() {
+    var result = validate($width.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $widthValidate.html('宽度' + result.reason);
+      $width.select();
+    } else {
+      $widthValidate.html('');
+    }
+  });
 
-$width.keypress(function(e) {
-  if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+  $width.keypress(function(e) {
+    if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
       e.preventDefault();
-              
-  }
-    
-});
+    }
+  });
 
-$height.focusout(function() {
-      var result = validate($height.val());
-      isPassValidate = result.isOK;
-      if(!result.isOK) {
-          $heightValidate.html('高度' + result.reason);
-              $height.select();
-                            
-      } else {
-          $heightValidate.html('');
-                      
-          }
-            
-});
+  $height.focusout(function() {
+    var result = validate($height.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $heightValidate.html('高度' + result.reason);
+      $height.select();
+    } else {
+      $heightValidate.html('');
+    }
+  });
+
+  $height.keypress(function(e) {
+    if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+      e.preventDefault();
+    }
+  });
 
   $btnCal.click(function(){
     if(!isPassValidate) return;
+
     var w = $width.val(),
         h = $height.val();
 
